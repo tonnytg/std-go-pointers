468ddcf211dbe066d650ac23dcc2b563370f147a
Antonio Thomacelli <tonnytg@gmail.com>
2021-05-06 23:01:31 -0300

    add a and b with diference values and position

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ func main()  {
 	fmt.Printf("The value I have now is: %v \n", a)
 	fmt.Printf("Memory of a is: %v \n", &a)
 
-	var ponteiro *int = &a
+	var pointer *int = &a
 
-	fmt.Printf("Valor do ponteiro é: %v \n", ponteiro)
+	fmt.Printf("Pointer value is: %v \n", pointer)
+	fmt.Println("Value of pointer is:", *pointer)
+
+	b := 11
+	pointer = &b
+
+	fmt.Printf("Now the memory position of pointer is %v \n", &pointer)
+	fmt.Printf("Now the value of a is %v and pointer is %v \n", *pointer, a)
 }
 
 
